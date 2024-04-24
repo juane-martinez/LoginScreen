949a957696939f9632583defbda5ce221679710c
juane-martinez <juanemartinez@unicauca.edu.co>
2024-04-24 15:15:29 -0500

    Login 1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,11 +34,12 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.volley)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
     implementation ("com.google.android.gms:play-services-mlkit-text-recognition:19.0.0")
     implementation ("com.airbnb.android:lottie:4.2.1")
-
+    implementation("com.android.volley:volley:1.2.1")
 }
